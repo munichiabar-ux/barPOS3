21d232a39e020cb885b4dd86ce938ded565fe15d
munichiabar-ux <munichiabar@gmail.com>
2025-09-17 20:09:07 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,7 @@
+const CACHE = 'barpos-v1';
+self.addEventListener('install', e=>{
+e.waitUntil(caches.open(CACHE).then(c=> c.addAll(['./','./index.html','./styles.css','./app.js','./icon-512.png','./manifest.webmanifest'])));
+});
+self.addEventListener('fetch', e=>{
+e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request)));
+});
