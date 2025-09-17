fc5ba51a5f10554c11822a3f7dcae17bfc4cc2e5
munichiabar-ux <munichiabar@gmail.com>
2025-09-17 20:46:46 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'barpos-v3';
+const CACHE = 'barpos-v4';
 self.addEventListener('install', e=>{
 e.waitUntil(caches.open(CACHE).then(c=> c.addAll(['./','./index.html','./styles.css','./app.js','./icon-512.png','./manifest.webmanifest'])));
 });
